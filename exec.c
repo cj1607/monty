b872df2bfb36d799ec74f8ca751dcbdd2d1019b1
cj1607 <ucheokorojefferson@gmail.com>
2022-12-15 05:49:54 -0800

    where the hell am i

--- exec.c
+++ exec.c
@@ -1,14 +1,14 @@
 #include "monty.h"
 
 /**
- * execute - function that executes the opcode
- * @stack: head stack linked list
- * @counter: line count
- * @file: pointer to monty file stream
- * @content: line content
- *
- * Return: nothing
- */
+* execute - function that executes the opcode
+* @stack: head stack linked list
+* @counter: line count
+* @file: pointer to monty file stream
+* @content: line content
+*
+* Return: nothing
+*/
 int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
   	instruction_t opst[] = {
